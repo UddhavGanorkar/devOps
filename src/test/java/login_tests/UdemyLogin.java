06b08c6505bf1e30ae7bac3aba2d44bdda53c072
Uddhav G <uddhav.ganorkar99@gmail.com>
2021-09-27 12:12:00 +0530

    Updated Commit

--- src/test/java/login_tests/UdemyLogin.java
+++ src/test/java/login_tests/UdemyLogin.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class UdemyLogin {
@@ -15,10 +16,7 @@ public class UdemyLogin {
 		driver.get("https://www.udemy.com/");
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[normalize-space()='Log in']")).click();
-		//driver.findElement(By.name("email")).sendKeys("admin@gmail.com");
-	//	driver.findElement(By.name("password")).sendKeys("admin123");
-		//driver.findElement(By.name("submit")).click();
-		//boolean x = driver.findElement(By.xpath("//div[contains(text(),'We found your account."))
+		Assert.assertEquals(driver.getTitle(),"Attention Required! | Cloudflare");
   }
   @BeforeTest
   public void beforeTest() {
